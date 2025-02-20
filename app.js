cb31c56c2f5c128162d1b3143a1488560c6e23de
Falsidical <franport@outlook.com>
2025-02-20 07:51:49 +0000

    Create Div logic

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
-const testBook = new Book('Fran', 'Test Book', 110, true);
-addBookToLibrary(testBook);
+const dialog = document.querySelector('dialog');
+const showBtn = document.querySelector('.show');
+const closeBtn = document.querySelector('.close');
+const form = document.querySelector('form');
+const booksContainer = document.querySelector('.books-container');
 
-const myLibrary = [];
+const icons = ['circle-outline.svg', 'square-edit-outline.svg', 'trash-can-outline.svg'];
+
+showBtn.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+  dialog.close();
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  for (const [name, value] of formData.entries()) {
+    console.log(name, value);
+  }
+  dialog.close();
+});
+
+function createBookDiv(book) {
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book');
+
+  const title = document.createElement('h3');
+  title.textContent = book.title;
+
+  const author = document.createElement('p');
+  author.textContent = book.author;
+
+  const iconsDiv = document.createElement('div');
+  iconsDiv.classList.add('icons');
+
+  for (let i = 0; i < 3; i++) {
+    const icon = document.createElement('img');
+    icon.src = `img/svg/${icons[i]}`;
+    iconsDiv.appendChild(icon);
+  }
+
+  bookDiv.appendChild(title);
+  bookDiv.appendChild(author);
+  bookDiv.appendChild(iconsDiv);
+
+  booksContainer.appendChild(bookDiv);
+}
+
+const myLibrary = [
+  {
+    title: 'Harry Potter',
+    author: 'J.K. Rowling',
+    read: true,
+  },
+  {
+    title: 'Games of Thrones',
+    author: 'George RR Martin',
+    read: false,
+  },
+];
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -13,3 +73,7 @@ function Book(author, title, pages, read) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+myLibrary.forEach((book) => {
+  createBookDiv(book);
+});
